f25cc16fd972a3842202702cae11054bb9d96e69
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-15 17:17:00 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  "SELECT Gpa FROM Students ORDER BY Gpa DESC;"
+  "SELECT FIRST(Gpa) FROM Students ORDER BY Gpa DESC;"
 end
 
 def lowest_student_gpa
